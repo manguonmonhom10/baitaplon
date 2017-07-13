c738b0d1f92b9d3bcfad65763e04181e1160e0fb
phiphi216 <nguyenxuanphi216@gmail.com>
2017-07-14 00:17:43 +0700

    lien_he

---
+++ lienhe.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="chongoicodinh.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<style>
+	body{
+		background-color:#FFF;
+		border:1px solid while;
+		}
+	body, table, td,tr,img, div,{border-radius:5px;}
+
+	#anh_chay{border:1px double #333333;}
+	.mid{
+		width: 900px;
+		height: 300px;
+		}
+			
+	/*-- Menu------*/
+
+		.wrapper{ 
+			width: 900px; 
+			height:50px; 
+			background: #F8F8F8;
+			margin:auto; }
+		ul{
+			margin:0;
+			padding:0;
+		}
+		li{
+			list-style-type:none;
+			float:left;
+			padding:10px;
+			}
+		li a{
+			display:block;
+			padding:10px;
+			text-decoration:none;
+			line-height:1px;
+			min-height:1px;
+			color:#F00;
+			}
+		li a:hover{
+			background-color:#FFF;
+			color:#000;
+		}
+		.sub_menu{
+			position:absolute;
+			display:none;
+		}
+		.menu:hover .sub_menu{
+			display:block;
+		}
+		.sub_menu li{
+			float:none;
+			}
+		
+	
+	
+</style>
+</head>
+
+<body>
+
+<table align="center">
+<tr align="right">
+	<td>
+    	<a><i class="fa fa-phone"> Hostline:0972280223</i></a> 
+    	<a><i class="fa fa-envelope"> Email:nguyenxuanphi216@gmail.com</i></a>  
+	</td>
+</tr>
+<tr>
+	<td > <img src="anh/anh1.jpg" width="946px" height="300px" /></td>
+</tr>
+<table bgcolor="#EAEAEA" align="center" width="946px" >
+    <div class="wrapper">
+    	 <ul>
+ 			<li><img src="anh/logo.jpg" width="66px" height="26px" /></li>
+         	<li><a href="trangchututao.php">Trang chủ</a></li>
+            <li><a href="#">Giới thiệu</a></li>
+            <li><a href="#">Văn phòng cho thuê</a></li>
+            <li><a href="#">Lịch đăng ký</a></li>              
+           	<li><a href="#">Liên hệ</a></li>
+            <li><a href="#">Đăng nhập</a></li>
+         </ul>
+     </div>
+
+<table bgcolor="#FFFFCC" align="center" width="800px">
+    <td>
+    	<img src="anh/lienhe3.jpg" style="width:250px; height:180px; float:right;" />
+        <h2>Thông tin liên hệ</h2>
+        <p>Địa chỉ:Đường số 5 Mai Xuân Thưởng, Vĩnh Hòa, tp. Nha Trang, Khánh Hòa </p>
+        <p>Điện thoại: 0972280223</p>
+        <p>Email: nguyenxuanphi216@gmail.com</p>
+        <a href=""><img src="anh/lienhe1.jpg" width="800px" height="500px"/></p>
+    </td>
+ </table>
+ <table align="center" width="1000px" bgcolor="#F2F2F2">
+ <td></td>
+</tr>
+</table>
+</body>
+</html>
