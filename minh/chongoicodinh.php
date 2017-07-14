8520927e8b8a237417b421fc9acc98ce702fd3f1
phamtanminh <Nguyenphucnguyen.mp@gmail.com>
2017-07-14 14:56:02 +0700

    cho ngoi co dinh

---
+++ minh/chongoicodinh.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+	body{
+		background-color:#FFF;}
+		p {
+    		text-indent: 20px;
+			}
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<table align="center">
+<tr align="right">
+	<td>
+    	<a><i class="fa fa-phone"> Hostline:0972280223</i></a> 
+    	<a><i class="fa fa-envelope"> Email:nguyenxuanphi216@gmail.com</i></a>  
+	</td>
+</tr>
+<tr>
+	<td > <img src="anh/anh1.jpg" width="900px" height="300px" /></td>
+</tr>
+<table bgcolor="#EAEAEA" align="center" width="900px" >
+	<tr> 
+    <td> <a><img src="anh/logo.jpg" width="100px" /></a>
+    <td> <a href="trangchututao.php"/a> Trang chủ</td>
+    <td> <a href="#"/a> Giới thiệu</td>
+    <td> <a href="#"/a> Văn phòng cho thuê</td>
+    <td> <a href="#"/a> Phòng họp cho thuê</td>
+    <td> <a href="#"/a> Liên hệ</td>
+</table>
+<table align="left"  width="220px">
+	<tr>
+    	<td>
+        </td>
+     </tr>   
+  
+</table> 
+</table>
+ <table align="right" bgcolor="#FFFF99" width="220px">      
+       
+    </tr>
+    <tr height="50px">
+    	
+        <td class="login-input">Từ Ngày<br /><input type="date" name="user" /></td>
+    </tr>
+    <tr height="50px">
+        <td class="login-input">Đến Ngày<br /><input type="date" name="pass" /></td>
+    </tr>
+    <tr height="50px">
+        <td align="left">Email<br /><input type="email" name="" /></td>
+    </tr>
+    <tr height="50px">
+        <td>Số điện thoại<br /><input type="tel" name="" /></td>
+    </tr>
+    <tr height="50px">
+        <td id="login-button"><input type="submit" name="submit_name" value="Đặt Phòng" /> <input type="reset" name="reset_name" value="Đặt Lại" /></td>
+    </tr>
+<table bgcolor="#FFFFCC" align="">
+
+    <td>
+    	<img src="anh/anh4.jpg" style="width:250px; height:180px; float:right" />
+        <h3 href="#">Chỗ ngồi cố định</h3>
+        <a href="#"></a>
+       <a>Chỗ ngồi cố định là một vị trí làm việc ổn định mà khách hàng thuê có thời hạn cố định và  sử dụng toàn thời gian.
+</a><br /><br />
+       <a>Đối tượng sử dụng phù hợp</a><br /><br />     
+       <a>Khi các công ty nước ngoài cần đặt văn phòng đại diện quy mô nhỏ tại Việt Nam hay tiến hành các hoạt động thăm dò thị trường, họ cần một không gian văn phòng vừa đủ cho một vài nhân viên làm việc toàn thời gian. Chỗ ngồi cố định là một sự lựa chọn thích hợp. Khi kết hợp đồng thời giữa chỗ ngồi cố định và các dịch vụ văn phòng ảo, bạn sẽ có ngay một văn phòng tiện nghi và hiện đại, phù hợp cho tổ chức của mình với chi phí tiết kiệm nhất. Thực tế, có rất nhiều văn phòng đại diện nước ngoài đã lựa chọn mô hình này và hoạt động rất hiệu quả.</a><br /><br />
+<a>Tương tự, văn phòng đại diện của các công ty từ tỉnh thành khác, doanh nghiệp nhỏ và vừa mới thành lập, nhóm dự án hoặc cá nhân hoạt động kinh doanh độc lập (freelancer) mong muốn tiết kiệm chi phí, tối ưu nhân sự để sử dụng các nguồn lực hiệu quả. Sử dụng dịch vụ chỗ ngồi cố định của chúng tôi sẽ giúp họ tập trung năng lực vào hoạt động kinh doanh cốt lõi của mình hiệu quả nhất.</a><br /><br />
+
+       <a>Tiện ích của chúng tôi cung cấp</a><br>
+       <a>Bạn có thể sử dụng các tiện ích khi sử dụng chỗ ngồi cố định tại chúng tôi:</a><br />
+       <p>Bàn ghế và tủ đựng hồ sơ cá nhân</p>
+   	   <p>  Miễn phí điều hoà</p>
+       <p>   Miễn phí sử dụng ADSL internet hay wifi</p>
+   	   <p> Miễn phí sử dụng khu vực tiếp khách chung</p>
+  	   <p> Hỗ trợ phòng họp tiếp khách</p><br />
+       <a>Ngoài ra, bạn có thể sử dụng kết hợp dịch vụ văn phòng ảo để có được một văn phòng làm việc hoàn thiện, đáp ứng nhu cầu 		          kinh doanh của mình.</a><br /><br />
+   	   <a>Chúng tôi hy vọng rằng dịch vụ này sẽ giúp bạn tiết kiệm thời gian quản lý và chi phí, đồng thời trở nên chuyên nghiệp    		trong hoạt động thực tiễn và sự cảm nhận của khách hàng.</a><br /><br />
+   	   <a>Chi phí cho một chỗ ngồi cố định tại tòa nhà của chúng tôi là 4.460.000 VNĐ/tháng.</a>
+       
+  </td>
+
+ </table>
+ 
+ 
+    
+ 
+</body>
+</html>
