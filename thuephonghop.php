31903e878dfa485f26e53745991cb180e23bf1cf
phiphi216 <nguyenxuanphi216@gmail.com>
2017-07-14 15:07:54 +0700

    thuePH

---
+++ thuephonghop.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+	body{
+		background-color:#FFF;
+		border:1px solid while;
+		}
+	body, table, td,tr,img, div,{border-radius:5px;}
+
+	#anh_chay{border:1px double #333333;}
+	.mid{
+		width: 900px;
+		height: 300px;
+		}
+			
+	/*-- Menu------*/
+	
+
+	/*
+	.main_menu{ background:#069; margin-top: 5px;
+	list-style-type:none;
+	text-align:center;}
+		
+	.main_menu ul li{
+		*display:inline;
+		color:#f1f1f1f;
+		display:inline-block;
+		width:160px;
+		height:30px;
+		line-height:40px;
+		text-align:center;
+		position:relative;
+		list-style:outside none none;	
+		}
+	
+	.main_menu ul li a{
+		color:#FFF;
+		text-decoration:none;}
+	.main_menu ul li a:hover{
+		background:#f1f1f1;
+		color:#CCC;}
+	.main_menu ul ul{
+		display:none;
+		}
+	.main_menu ul li:hover ul{
+		display:block;
+		position:absolute;
+		background:#F00
+		left:0;
+		top:100%;
+		}	
+		*/
+		.wrapper{ 
+			width: 900px; 
+			height:50px; 
+			background: #F8F8F8;
+			margin:auto; }
+		ul{
+			margin:0;
+			padding:0;
+		}
+		li{
+			list-style-type:none;
+			float:left;
+			padding:10px;
+			}
+		li a{
+			display:block;
+			padding:10px;
+			text-decoration:none;
+			line-height:1px;
+			min-height:1px;
+			color:#F00;
+			}
+		li a:hover{
+			background-color:#FFF;
+			color:#000;
+		}
+		.sub_menu{
+			position:absolute;
+			display:none;
+		}
+		.menu:hover .sub_menu{
+			display:block;
+		}
+		.sub_menu li{
+			float:none;
+			}
+		p {
+    		text-indent: 20px;
+			}
+		
+	
+	
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<table align="center">
+<tr align="right">
+	<td>
+    	<a><i class="fa fa-phone"> Hostline:0972280223</i></a> 
+    	<a><i class="fa fa-envelope"> Email:nguyenxuanphi216@gmail.com</i></a>  
+	</td>
+</tr>
+<tr>
+	<td> <img src="anh/anh1.jpg" width="900px" height="200px" /></td>
+</tr>
+<table bgcolor="#EAEAEA" align="center" width="900px" >
+    <div class="wrapper">
+    	 <ul>
+ 			<li><img src="anh/logo.jpg" width="80px" height="25px" /></li>
+         	<li><a href="trangchututao.php">Trang chủ</a></li>
+            <li><a href="">Giới thiệu</a></li>
+            <li class="menu" ><a href="">Dịch vụ</a></li>
+            	<ul class="sub_menu">
+               		<li><a href="">Phòng họp cho thuê</a></li>
+                	<li><a href="">Văn phòng cho thuê</a></li>
+                </ul>               
+           	<li><a href="">Liên hệ</a></li>
+            <li><a href="">Lịch đăng ký</a></li>
+            <li><a href="">Thành viên</a></li>
+         </ul>
+              
+        </div>
+
+<table align="left"  width="220px">
+	<tr>
+    	<td>
+        </td>
+     </tr>   
+  
+</table> 
+</table>
+<table align="right" bgcolor="#FFFF99" width="220px">      
+       
+    </tr>
+    <tr height="50px">
+    	
+        <td class="login-input">Từ Ngày<br /><input type="date" name="user" /></td>
+    </tr>
+    <tr height="50px">
+        <td class="login-input">Đến Ngày<br /><input type="date" name="pass" /></td>
+    </tr>
+    <tr height="50px">
+        <td align="left">Email<br /><input type="email" name="" /></td>
+    </tr>
+    <tr height="50px">
+        <td>Số điện thoại<br /><input type="tel" name="" /></td>
+    </tr>
+    <tr height="50px">
+        <td id="login-button"><input type="submit" name="submit_name" value="Đặt Phòng" /> <input type="reset" name="reset_name" value="Đặt Lại" /></td>
+    </tr> 
+
+<table bgcolor="#FFFFCC" align="center">
+	<tr >
+    <td >
+    
+    	<img src="anh/anh7.jpg" style="width:400px; height:200px; float:right" />
+        <h3 href="#">Thuê phòng họp</h3>
+        <a href="#"></a>
+       
+       
+       <a>SOffice cho thuê phòng họp với nhiều phòng có sức chứa khác nhau, đáp ứng từng yêu cầu của quý khách hàng. Phòng họp được trang bị các loại máy chiếu (projector) sắc nét và có độ phân giải cao, Internet tốc độ cao, Wifi và hệ thống đàm thoại hội nghị.</a><br /><br />
+       
+       <a>Đối tượng sử dụng phòng họp cho thuê</a><br /><br />
+       <a>
+Khi quý khách hàng cần tổ chức những buổi hội nghị khách hàng, đại hội cổ đông, và các hình thức meeting mang tính chuyên nghiệp, thuê một phòng họp tại SOffice là một giải pháp hợp lí với chi phí tiết kiệm..</a><br /><br />
+       <a>Tiện ích của  phòng hop cho thuê ?</a><br>
+       <a>Phòng họp được trang bị đầy đủ các phương tiện:</a><br />
+         <p>  Máy chiếu (projector) sắc nét và có độ phân giải cao</a><br />
+   			<p>  Internet tốc độ cao</p>
+    		<p>   Wifi</p>
+  		 <p>   Hệ thống đàm thoại hội nghị.</p><br />
+   
+   <a> Chúng tôi còn phục vụ các dịch vụ khác đi kèm như: café, hoa quả, nước uống, bánh ngọt… khi có yêu cầu.</a><br />
+   <a>Quy mô 4-12 người, Tùy diện tích phòng.</a><br />
+   <a>10-12 người: 200.000 Vnđ/giờ</a><br />
+   <a>4-6 người: 1000.000 Vnđ/giờ</a>
+       
+  </td>
+
+	</tr> 
+</table>
+
+ </table>
+ </body>
+</html>
