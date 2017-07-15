6d0fcf2aac53ef432b832156479fad14daede25a
phiphi216 <nguyenxuanphi216@gmail.com>
2017-07-16 02:18:48 +0700

    QL

---
+++ QLTP/menuForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTP
+{
+    public partial class menuForm : Form
+    {
+        public menuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void phòngToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            phongForm ph = new phongForm();
+            ph.Show();
+        }
+
+        private void kháchHàngToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            khachhangForm ph = new khachhangForm();
+            ph.Show();
+        }
+
+        private void đăngKíToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            dangkiForm ph = new dangkiForm();
+            ph.Show();
+        }
+
+        private void kháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            khachhangForm ph = new khachhangForm();
+            ph.Show();
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
