d2feb602713d9b96e6ddd8d6dc2b07e04514100a
phamtanminh <Nguyenphucnguyen.mp@gmail.com>
2017-07-15 15:27:33 +0700

    giới thiệu người dùng

---
+++ minh/gioithieunguoidung.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+	body{
+		background-color:#FFF;
+		border:1px solid while;
+		}
+	body, table, td,tr,img, div,{border-radius:5px;}
+
+	#anh_chay{border:1px double #333333;}
+	.mid{
+		width: 900px;
+		height: 300px;
+		}
+			
+	/*-- Menu------*/
+	
+
+	/*
+	.main_menu{ background:#069; margin-top: 5px;
+	list-style-type:none;
+	text-align:center;}
+		
+	.main_menu ul li{
+		*display:inline;
+		color:#f1f1f1f;
+		display:inline-block;
+		width:160px;
+		height:30px;
+		line-height:40px;
+		text-align:center;
+		position:relative;
+		list-style:outside none none;	
+		}
+	
+	.main_menu ul li a{
+		color:#FFF;
+		text-decoration:none;}
+	.main_menu ul li a:hover{
+		background:#f1f1f1;
+		color:#CCC;}
+	.main_menu ul ul{
+		display:none;
+		}
+	.main_menu ul li:hover ul{
+		display:block;
+		position:absolute;
+		background:#F00
+		left:0;
+		top:100%;
+		}	
+		*/
+		.wrapper{ 
+			width: 900px; 
+			height:50px; 
+			background: #F8F8F8;
+			margin:auto; }
+		ul{
+			margin:0;
+			padding:0;
+		}
+		li{
+			list-style-type:none;
+			float:left;
+			padding:10px;
+			}
+		li a{
+			display:block;
+			padding:10px;
+			text-decoration:none;
+			line-height:1px;
+			min-height:1px;
+			color:#F00;
+			}
+		li a:hover{
+			background-color:#FFF;
+			color:#000;
+		}
+		.sub_menu{
+			position:absolute;
+			display:none;
+		}
+		.menu:hover .sub_menu{
+			display:block;
+		}
+		.sub_menu li{
+			float:none;
+			}
+		p {
+    		text-indent: 20px;
+			}
+		
+	
+	
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body> 
+<table bgcolor="#FFFFCC" align="center" width="660px" border="1px">
+	<tr>
+    <td>
+    <a><h3>Giới thiệu chung</h3></a>
+
+        <a href="trangchututao.php"></a>
+        <b>Văn phòng  tại NHA TRANG</b><br />
+        <a>Đối với doanh nghiệp, trụ sở văn phòng thuận tiện và phù hợp luôn là mối bận tâm hàng đầu. Đặt văn phòng ở đâu? Diện           tích bao nhiêu và không gian như thế nào luôn là những câu hỏi được đặt ra để sao cho vừa phù hợp lĩnh vực hoạt động           cũng như khả năng tài chính. Hiểu được thực tế này, chúng tôi đã ra đời với mong muốn tiết kiệm thời gian và nguồn lực           cho doanh nghiệp. Tại đây chúng tôi đưa ra các gói tư vấn miễn phí tìm kiếm văn phòng, đàm phán giá, chuẩn bị hợp đồng           & tư vấn những ý tưởng trong việc bố trí mặt bằng & thi công văn phòng. Các gói dịch vụ của chúng tôi gồm:</a>
+        <p>- Tư vấn thuê - cho thuê văn phòng hạng A, B, C, Văn phòng giá rẻ…</p>
+        <p>- Tư  vấn văn phòng Ảo – văn phòng Trọn gói – phòng họp</p>
+        <a>Chính sách phí áp dụng cho khách hàng:</a>
+        <p>-   Chúng tôi miễn phí hoàn toàn phí dịch vụ tư vấn, môi giới cho các khách hàng có nhu cầu thuê văn phòng và cam kết         mang lại sự hài lòng cao nhất cho Quý khách hàng.</p>
+		<p>-    Khuyến mại 01 gói Tư vấn Phong thủy cho văn phòng đối với các hợp đồng có giá trị trên 30 triệu và thời gian trên 			              3 năm.</p>
+     </td>
+     </tr>
+</table>
+</body>
