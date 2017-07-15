3c996d1129612b74b315bdb6f6368dac7872b903
levankhue <khuelv.96@gmail.com>
2017-07-15 12:25:40 +0700

    trangchu

---
+++ trangchu.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="trangchututao.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<style>
+@charset "utf-8";
+/* CSS Document */
+	body{
+		background-color:#FFF;
+		border:1px solid while;
+		}
+	body, table, td,tr,img, div,{border-radius:5px;}
+
+	#anh_chay{border:1px double #333333;}
+	.mid{
+		width: 1000px;
+		height: 300px;
+		}			
+	/*-- Menu------*/
+		.wrapper{ 
+			width: 1000px; 
+			height:50px; 
+			background: #F8F8F8;
+			margin:auto; }
+		ul{
+			margin:0;
+			padding:0;
+		}
+		li{
+			list-style-type:none;
+			float:left;
+			padding:10px;
+			}
+		li a{
+			display:block;
+			padding:10px;
+			text-decoration:none;
+			line-height:1px;
+			min-height:1px;
+			color:#F00;
+			}
+		li a:hover{
+			background-color:#FFF;
+			color:#000;
+		}
+		.sub_menu{
+			position:absolute;
+			display:none;
+		}
+		.menu:hover .sub_menu{
+			display:block;
+		}
+		.sub_menu li{
+			float:none;
+			}
+		<!------------!>
+	
+	
+</style>
+</head>
+
+<body>
+
+<table align="center">
+<tr align="right">
+	<td>
+    	<a><i class="fa fa-phone"> Hostline:0972280223</i></a> 
+    	<a><i class="fa fa-envelope"> Email:nguyenxuanphi216@gmail.com</i></a>  
+	</td>
+</tr>
+<tr>
+	<td> <img src="anh/anh1.jpg" width="1000px" height="200px" /></td>
+</tr>
+<table bgcolor="#EAEAEA" align="center" width="1000px" >
+    <div class="wrapper">
+    	 <ul>
+ 			<li><img src="anh/logo.jpg" width="145px" height="26px" /></li>
+         	<li><a href="trangchututao.php">Trang chủ</a></li>
+            <li><a href="gioithieu.php">Giới thiệu</a></li>
+            <li><a href="quanlyvanphong.php">Văn phòng cho thuê</a></li>             
+           	<li><a href="lienhe.php">Liên hệ</a></li>
+            <li><a href="dangnhap.php">Đăng nhập</a></li>
+            <li><a href="dangky.php">Đăng ký</a></li> 
+         </ul>
+     </div>
+</table>
+<table align="center" class="mid" >
+<tr><td>
+<div id="anh_chay">
+<marquee direction="left" behavior="alternate" onmouseover="this.stop()" onmouseout="this.start()">
+     <img src="anh/anh2.jpg" width="900px" height="300px" />
+     <img src="anh/anh3.jpg" width="900px" height="300px" />
+     <img src="anh/anh4.jpg" width="900px" height="300px" />
+    </marquee>
+    </div></td>
+</tr>
+</table>
+<table align="center" border="1px solid black"  >
+    <td width="55px" >
+    	<a href="thuephonghop.php"><img src="anh/anh2.jpg" width="176px" height="140px" /><br>
+        <h3 href="thuephonghop.php">Thuê văn phòng</h3><br>
+        <a>Chỗng ngồi làm việc thoải mái, không gian sang trọng, tiện nghi cao cấp...</a>
+    </td>
+    <td width="55px">
+    	<a href="chongoicodinh.php"><img src="anh/anh6.jpg" width="172px" height="140px" /><br/>
+        <h3 href="chongoicodinh.php">Chỗ cố định</h3><br/>
+        <a width="40px" >Một phần quan trọng xây dựng hình ảnh cho doanh nghiệp của bạn...</a>
+    </td>
+    <td width="55px">
+    	<a href="chongoilinhhoat.php"><img src="anh/anh5.jpg" width="172px" height="140px" /><br/>
+        <h3 href="#">Chỗ ngồi linh hoạt</h3><br />
+        <a>Chi phí thuê phòng họp phù hợp với điều kiện làm việc...</a>
+    </td>
+     <td width="55px">
+    	<a href="phonghoinghi.php"><img src="anh/anh7.jpg" width="172px" height="140px" /><br/>
+        <h3 href="phonghoinghi.php">Phòng hội nghị</h3><br />
+        <a>Phòng họp sang trọng,trang thiết bị đầy đủ đáp ứng nhu cầu...</a>
+    </td>
+    <td width="55px">
+    	<a href="vanphongao.php"><img src="anh/anh6.jpg" width="174px" height="140px" /><br/>
+        <h3 href="#">Văn phòng ảo</h3><br/>
+        <a width="40px" >Văn phòng ảo sẻ giúp bạn giảm chi phí và hoạt động kinh bình thường...  </a>
+    </td>
+</table>
+<table align="center" width="1000px" bgcolor="#F2F2F2">
+    <td align="center">Trung tâm văn phòng</td>
+    <td></td>
+</tr>
+</table>
+</body>
+</html>
+
