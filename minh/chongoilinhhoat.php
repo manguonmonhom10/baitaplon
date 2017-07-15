1bed4add5c4e8221d79839e133779b909ffa7e34
phamtanminh <Nguyenphucnguyen.mp@gmail.com>
2017-07-15 14:21:54 +0700

    cho ngoi linh hoat

---
+++ minh/chongoilinhhoat.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+	body{
+		background-color:#FFF;
+		border:1px solid while;
+		}
+	body, table, td,tr,img, div,{border-radius:5px;}
+
+	#anh_chay{border:1px double #333333;}
+	.mid{
+		width: 900px;
+		height: 300px;
+		}
+			
+	/*-- Menu------*/
+	
+
+	/*
+	.main_menu{ background:#069; margin-top: 5px;
+	list-style-type:none;
+	text-align:center;}
+		
+	.main_menu ul li{
+		*display:inline;
+		color:#f1f1f1f;
+		display:inline-block;
+		width:160px;
+		height:30px;
+		line-height:40px;
+		text-align:center;
+		position:relative;
+		list-style:outside none none;	
+		}
+	
+	.main_menu ul li a{
+		color:#FFF;
+		text-decoration:none;}
+	.main_menu ul li a:hover{
+		background:#f1f1f1;
+		color:#CCC;}
+	.main_menu ul ul{
+		display:none;
+		}
+	.main_menu ul li:hover ul{
+		display:block;
+		position:absolute;
+		background:#F00
+		left:0;
+		top:100%;
+		}	
+		*/
+		.wrapper{ 
+			width: 900px; 
+			height:50px; 
+			background: #F8F8F8;
+			margin:auto; }
+		ul{
+			margin:0;
+			padding:0;
+		}
+		li{
+			list-style-type:none;
+			float:left;
+			padding:10px;
+			}
+		li a{
+			display:block;
+			padding:10px;
+			text-decoration:none;
+			line-height:1px;
+			min-height:1px;
+			color:#F00;
+			}
+		li a:hover{
+			background-color:#FFF;
+			color:#000;
+		}
+		.sub_menu{
+			position:absolute;
+			display:none;
+		}
+		.menu:hover .sub_menu{
+			display:block;
+		}
+		.sub_menu li{
+			float:none;
+			}
+		p {
+    		text-indent: 20px;
+			}
+	
+	
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<table align="center">
+<tr align="right">
+	<td>
+    	<a><i class="fa fa-phone"> Hostline:0972280223</i></a> 
+    	<a><i class="fa fa-envelope"> Email:nguyenxuanphi216@gmail.com</i></a>  
+	</td>
+</tr>
+<tr>
+	<td> <img src="anh/anh1.jpg" width="900px" height="200px" /></td>
+</tr>
+<table bgcolor="#EAEAEA" align="center" width="900px" >
+    <div class="wrapper">
+    	 <ul>
+ 			<li><img src="anh/logo.jpg" width="80px" height="25px" /></li>
+         	<li><a href="trangchututao.php">Trang chủ</a></li>
+            <li><a href="">Giới thiệu</a></li>
+            <li class="menu" ><a href="">Dịch vụ</a></li>
+            	<ul class="sub_menu">
+               		<li><a href="">Phòng họp cho thuê</a></li>
+                	<li><a href="">Văn phòng cho thuê</a></li>
+                </ul>               
+           	<li><a href="">Liên hệ</a></li>
+            <li><a href="">Lịch đăng ký</a></li>
+            <li><a href="">Thành viên</a></li>
+         </ul>
+              
+        </div>
+
+<table align="left"  width="220px">
+	<tr>
+    	<td>
+        </td>
+     </tr>   
+  
+</table> 
+</table>
+<table align="right" bgcolor="#FFFF99" width="220px">      
+       
+    </tr>
+    <tr height="50px">
+    	
+        <td class="login-input">Từ Ngày<br /><input type="date" name="user" /></td>
+    </tr>
+    <tr height="50px">
+        <td class="login-input">Đến Ngày<br /><input type="date" name="pass" /></td>
+    </tr>
+    <tr height="50px">
+        <td align="left">Email<br /><input type="email" name="" /></td>
+    </tr>
+    <tr height="50px">
+        <td>Số điện thoại<br /><input type="tel" name="" /></td>
+    </tr>
+    <tr height="50px">
+        <td id="login-button"><input type="submit" name="submit_name" value="Đặt Phòng" /> <input type="reset" name="reset_name" value="Đặt Lại" /></td>
+    </tr> 
+
+<table bgcolor="#FFFFCC" align="center">
+	<tr >
+    <td >
+    
+    	<img src="anh/anh5.jpg" style="width:250px; height:180px; float:right" />
+        <h3 href="#">Chỗ ngồi linh hoạt</h3>
+        <a href="#"></a>
+       <b>Chỗ ngồi linh hoạt là gì?</b><br /><br />
+       
+       <a>Chỗ ngồi linh hoạt là một vị trí làm việc mà khách hàng thuê theo giờ trên tháng thay vì toàn thời gian.</a><br /><br />
+       
+       <b>Đối tượng sử dụng phù hợp</b><br /><br />
+       <a>
+Với tính chất linh hoạt về thời gian sử dụng, dịch vụ này đáp ứng nhu cầu của các thương gia thường xuyên đi công tác; cá nhân hoạt động kinh doanh độc lập với nhu cầu chỗ ngồi không cố định và thời lượng sử dụng không nhiều. Điều này giúp bạn tối ưu hóa chi phí mà một văn phòng truyền thống không thể nào thực hiện được.</a><br /><br />
+       <a>Chúng tôi hy vọng sẽ mang lại cho quý doanh nghiệp vị trí làm việc tiện nghi với đầy đủ các trang thiết bị sử dụng chung/riêng và không gian làm việc chuyên nghiệp.</a><br>
+       <b>Chi phí để sử dụng chỗ ngồi linh hoạt sẽ giảm dần theo thời lượng thuê/tháng:</b><br /><br />
+       <p>	10giờ/tháng:460.000 Vnđ/tháng</p>
+       <p>	20giờ/tháng:690.000 Vnđ/tháng</p> 
+       <p>	40giờ/tháng:1.160.000 Vnđ/tháng</p> 
+       
+  </td>
+</tr>
+
+
+ </table>
+ 
+ </body>
+</html>
