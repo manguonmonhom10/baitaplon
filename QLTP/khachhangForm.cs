0941fcd1d6fb1a5df336cc0fe46604dba68e6b37
levankhue <khuelv.96@gmail.com>
2017-07-16 13:17:57 +0700

    khachhang form

--- QLTP/khachhangForm.cs
+++ QLTP/khachhangForm.cs
@@ -38,20 +38,25 @@ namespace QLTP
 
         private void button1_Click(object sender, EventArgs e)
         {
-           /* khachhang kh = new khachhang();
-            kh.setHoTen(textb
-            kh.s(Int32.Parse(textBox3.Text));
-            kh.setTinhTrang(textBox1.Text);
-            kh.setLoaiPhong(textBox4.Text);
+            khachhang ph = new khachhang();
+            ph.setHoTen(textBox1.Text);
+            ph.setDiaChi(textBox2.Text);
+            ph.setSDT(textBox3.Text);
+            ph.setMaPhong(textBox4.Text);
+            ph.setNgayThue(dateTimePicker1.Value.ToString("yyyy-MM-dd"));
+            ph.setNgayTra(dateTimePicker2.Value.ToString("yyyy-MM-dd"));
 
             khachhangBusiness pb = new khachhangBusiness();
-            kh.setMaPhong(textBox6.Text);
-            int ms = pb.themkhachhang(kh);
+            ph.setHoTen(textBox1.Text);
+            int ms = pb.them(ph);
             if (ms == 1)
             {
                 MessageBox.Show("Them thanh cong");
                 loadData();
-            }*/
+            }
+            
+            
+           
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -62,13 +67,45 @@ namespace QLTP
             textBox2.Text = row.Cells[1].Value.ToString();
             textBox3.Text = row.Cells[2].Value.ToString();
             textBox4.Text = row.Cells[3].Value.ToString();
-            textBox5.Text = row.Cells[4].Value.ToString();
-            textBox6.Text = row.Cells[5].Value.ToString();
+            dateTimePicker1.Value = DateTime.Parse(row.Cells[4].Value.ToString());
+            dateTimePicker2.Value = DateTime.Parse(row.Cells[5].Value.ToString());
         }
 
         private void khachhangForm_Load(object sender, EventArgs e)
         {
             loadData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            khachhang ph = new khachhang();
+            ph.setHoTen(textBox1.Text);
+            khachhangBusiness pb = new khachhangBusiness();
+            int rm = pb.xoa(ph);
+            if (rm == 1)
+            {
+                MessageBox.Show("Xoa thanh cong");
+                loadData();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            khachhang ph = new khachhang();
+            ph.setHoTen(textBox1.Text);
+            ph.setDiaChi(textBox2.Text);
+            ph.setSDT(textBox3.Text);
+            ph.setMaPhong(textBox4.Text);
+            ph.setNgayThue(dateTimePicker1.Value.ToString("yyyy-MM-dd"));
+            ph.setNgayTra(dateTimePicker2.Value.ToString("yyyy-MM-dd"));
+
+            khachhangBusiness pb = new khachhangBusiness();
+            int sa = pb.sua(ph);
+            if (sa == 1)
+            {
+                MessageBox.Show("Sua thanh cong");
+                loadData();
+            }
         }
     }
 }
