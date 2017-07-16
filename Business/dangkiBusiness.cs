ec2b44975dbf3e985c14581a47ee481849a75e82
phamtanminh <Nguyenphucnguyen.mp@gmail.com>
2017-07-16 15:27:11 +0700

    dangkiBusiness

---
+++ Business/dangkiBusiness.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Business
+{
+   public partial class dangkiBusiness
+    {
+        public DataTable hienthidangki()
+        {
+            try
+            {
+                dangki dk = new dangki();
+                return dk.getTable();
+            }
+            catch
+            { throw; }
+
+        }
+       // dat etne ham trung ten lop
+        public int themdangki(dangki dk)
+        {
+            return dk.themdangki(dk);
+        }
+    }
+}
