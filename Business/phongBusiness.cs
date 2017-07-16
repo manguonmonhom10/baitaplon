40ad8df60dd18833abc88ba22570ea8d66418693
levankhue <khuelv.96@gmail.com>
2017-07-16 16:21:53 +0700

    Business Phong

---
+++ Business/phongBusiness.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Business
+{
+    public partial class phongBusiness
+    {
+        //phong da = new phong();
+        // vieest ham lay thong va dua len datagrivew
+        public DataTable hienthiphong()
+        {
+            try
+            {
+                phong ph = new phong();
+                return ph.getTable();
+            }
+            catch
+            { throw; }
+
+        }
+        
+        public int them(phong ph)
+        {
+            return ph.themPhong(ph);
+        }
+        public int xoa(phong ph)
+        {
+            return ph.xoaPhong(ph);
+        }
+        public int sua(phong ph)
+        {
+            return ph.sua(ph);
+        }
+
+    }
+}
