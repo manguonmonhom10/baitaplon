8e391db603beb050c1339bdfac3354b50b0814c6
phamtanminh <Nguyenphucnguyen.mp@gmail.com>
2017-07-16 15:29:41 +0700

    khachhangBusiness

---
+++ Business/khachhangBusiness.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Business
+{
+   public partial class khachhangBusiness
+    {
+       public DataTable hienthikhachhang()
+       {
+           try
+           {
+               khachhang kh = new khachhang();
+               return kh.getTable();
+           }
+           catch
+           { throw; }
+
+       }
+       public int them(khachhang kh)
+       {
+           return kh.themkhachhang(kh);
+       }
+       public int xoa(khachhang kh)
+       {
+           return kh.xoakhachhang(kh);
+       }
+       public int sua(khachhang kh)
+       {
+           return kh.suakhachhang(kh);
+       }
+       
+    }
+}
