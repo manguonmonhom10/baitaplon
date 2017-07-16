f4f90816c0223f333ddce5765f39c8d689d8aeab
levankhue <khuelv.96@gmail.com>
2017-07-16 16:26:17 +0700

    khach hang Business

---
+++ Business/khachhangBusiness.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Business
+{
+   public partial class khachhangBusiness
+    {
+       public DataTable hienthikhachhang()
+       {
+           try
+           {
+               khachhang ph = new khachhang();
+               return ph.getTable();
+           }
+           catch
+           { throw; }
+
+       }
+       public int them(khachhang ph)
+       {
+           return ph.themkhachhang(ph);
+       }
+       public int xoa(khachhang ph)
+       {
+           return ph.xoakhachhang(ph);
+       }
+       public int sua(khachhang ph)
+       {
+           return ph.suakhachhang(ph);
+       }
+       
+    }
+}
